d984700c62937d11f8b390635c5d199bc5d39476
hagai-lvi <hagai.lvi@gmail.com>
2016-05-20 12:59:38 +0300

    Add ngramTo2gram and sort2gram

--- dsp-assignment-2-mapreduce/src/main/java/dsp/TokenizerMapper.java
+++ dsp-assignment-2-mapreduce/src/main/java/dsp/TokenizerMapper.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class TokenizerMapper
 		extends Mapper<Object, Text, Text, IntWritable> {
@@ -26,6 +26,52 @@ public class TokenizerMapper
 
 			word.set(currentWord);
 			context.write(word, one);
+		}
+	}
+
+	/**
+	 * Return a list of 2grams from the given ngram.
+	 * If ngram is a 1-gram, then returns an empty list
+	 * @throws AssertionError if the string is not of the expected format
+	 */
+	List<String> ngramTo2gram(String ngram) {
+		String[] split = ngram.split(" ");
+
+		assert split.length >= 1;
+
+		if (split.length == 1) {
+			// 1grams are ignored
+			return new LinkedList<String>();
+		}
+		else if (split.length == 2) {
+			String sorted = sort2gram(ngram);
+			return Collections.singletonList(sorted);
+		}
+		else {
+			int middle = split.length / 2;
+			List<String> res = new LinkedList<String>();
+
+			for (int i = 0 ; i < split.length ; i++ ){
+				if (i != middle) {
+					res.add(sort2gram(split[i] + " " + split[middle]));
+				}
+			}
+			return res;
+		}
+	}
+
+	/**
+	 * sorts a 2 gram lexicographically
+	 * @throws AssertionError if this is not a 2gram
+	 */
+	String sort2gram(String twoGram) {
+		String[] split = twoGram.split(" ");
+		assert split.length == 2;
+		if (split[0].compareTo(split[1]) < 0 ){
+			return split[0] + " " + split[1];
+		}
+		else {
+			return split[1] + " " + split[0];
 		}
 	}
 }
--- dsp-assignment-2-mapreduce/src/test/java/dsp/TestTokenizerMapper.java
+++ dsp-assignment-2-mapreduce/src/test/java/dsp/TestTokenizerMapper.java
@@ -3,17 +3,20 @@ package dsp;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TestTokenizerMapper {
 	private MapDriver<Object, Text, Text, IntWritable> mapDriver;
+	private TokenizerMapper mapper;
 
 	@Before
 	public void setUp() {
-		TokenizerMapper mapper = new TokenizerMapper();
+		mapper = new TokenizerMapper();
 		mapDriver = MapDriver.newMapDriver(mapper);
 	}
 
@@ -22,6 +25,58 @@ public class TestTokenizerMapper {
 		mapDriver.withInput(new IntWritable(1), new Text("abc"));
 		mapDriver.withOutput(new Text("abc"), new IntWritable(1));
 		mapDriver.runTest();
+	}
+
+	@Test
+	public void testSort2Gram() {
+
+		Assert.assertEquals("aaa aaa", mapper.sort2gram("aaa aaa"));
+
+		Assert.assertEquals("aaa bbb", mapper.sort2gram("aaa bbb"));
+
+		Assert.assertEquals("aaa bbb", mapper.sort2gram("bbb aaa"));
+	}
+
+
+	@Test
+	public void testOneGram() {
+		Assert.assertEquals(0, mapper.ngramTo2gram("aaa").size());
+	}
+
+	@Test
+	public void testTwoGram() {
+		List<String> twoGrams = mapper.ngramTo2gram("aaa bbb");
+		Assert.assertEquals(1, twoGrams.size());
+		Assert.assertTrue(twoGrams.contains("aaa bbb"));
+	}
+
+	@Test
+	public void testThreeGram() {
+		List<String> twoGrams = mapper.ngramTo2gram("aaa bbb ccc");
+		Assert.assertEquals(2, twoGrams.size());
+		Assert.assertTrue(twoGrams.contains("aaa bbb"));
+		Assert.assertTrue(twoGrams.contains("bbb ccc"));
+	}
+
+	@Test
+	public void testFourGram() {
+		List<String> twoGrams = mapper.ngramTo2gram("aaa bbb ccc ddd");
+		Assert.assertEquals(3, twoGrams.size());
+		Assert.assertTrue(twoGrams.contains("aaa ccc"));
+		Assert.assertTrue(twoGrams.contains("bbb ccc"));
+		Assert.assertTrue(twoGrams.contains("ccc ddd"));
+	}
+
+	@Test
+	public void testFiveGram() {
+		List<String> twoGrams = mapper.ngramTo2gram("aaa bbb ccc ddd eee");
+
+		Assert.assertEquals(4, twoGrams.size());
+
+		Assert.assertTrue(twoGrams.contains("aaa ccc"));
+		Assert.assertTrue(twoGrams.contains("bbb ccc"));
+		Assert.assertTrue(twoGrams.contains("ccc ddd"));
+		Assert.assertTrue(twoGrams.contains("ccc eee"));
 	}
 
 }
