4100300e1f977f082e207bd0d61e00270b48c704
hagai-lvi <hagai.lvi@gmail.com>
2016-06-15 16:33:10 +0300

    Added mapreduce test for topn stage

--- dsp-assignment-2-mapreduce-find-top/src/test/java/dsp/topN/TopNMapReduceTest.java
+++ dsp-assignment-2-mapreduce-find-top/src/test/java/dsp/topN/TopNMapReduceTest.java
@@ -27,12 +27,55 @@ public class TopNMapReduceTest {
 	}
 
 	@Test
-	public void testMapReduce() throws IOException {
+	public void testMapReduceSameDecade() throws IOException {
 		mapReduceDriver.getConfiguration().setInt("N",2);
-		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa bbb\t10\taaa\t12787\tbbb\t12787\t3.9316017295349717\n"));
-		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ccc\t10\taaa\t12787\tccc\t12787\t4.9316017295349717\n"));
-		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ddd\t10\taaa\t12787\tddd\t12787\t5.9316017295349717\n"));
-		System.out.println(mapReduceDriver.run());
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa bbb\t10\taaa\t12787\tbbb\t12787\t3.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ccc\t10\taaa\t12787\tccc\t12787\t4.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ddd\t10\taaa\t12787\tddd\t12787\t5.9316017295349717"));
+		mapReduceDriver.withOutput(new Text("1900\t5.9316017295349717\taaa ddd"),new Text(""));
+		mapReduceDriver.withOutput(new Text("1900\t4.9316017295349717\taaa ccc"),new Text(""));
+		mapReduceDriver.runTest();
+	}
+
+	@Test
+	public void testMapReduceMultipleDecades() throws IOException {
+		mapReduceDriver.getConfiguration().setInt("N",2);
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa bbb\t10\taaa\t12787\tbbb\t12787\t2.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ccc\t10\taaa\t12787\tccc\t12787\t1.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ddd\t10\taaa\t12787\tddd\t12787\t0.9316017295349717"));
+
+		mapReduceDriver.withInput(new Text(""), new Text("1910\taaa bbb\t10\taaa\t12787\tbbb\t12787\t100.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1910\taaa ccc\t10\taaa\t12787\tccc\t12787\t200.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1910\taaa ddd\t10\taaa\t12787\tddd\t12787\t300.9316017295349717"));
+
+		mapReduceDriver.withOutput(new Text("1900\t2.9316017295349717\taaa bbb"),new Text(""));
+		mapReduceDriver.withOutput(new Text("1900\t1.9316017295349717\taaa ccc"),new Text(""));
+
+		mapReduceDriver.withOutput(new Text("1910\t300.9316017295349717\taaa ddd"),new Text(""));
+		mapReduceDriver.withOutput(new Text("1910\t200.9316017295349717\taaa ccc"),new Text(""));
+
+		mapReduceDriver.runTest();
+	}
+
+	@Test
+	public void testMapReduceMultipleDecadesNegativePmi() throws IOException {
+		mapReduceDriver.getConfiguration().setInt("N",2);
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa bbb\t10\taaa\t12787\tbbb\t12787\t-2.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ccc\t10\taaa\t12787\tccc\t12787\t-1.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1900\taaa ddd\t10\taaa\t12787\tddd\t12787\t-0.9316017295349717"));
+
+		mapReduceDriver.withInput(new Text(""), new Text("1910\taaa bbb\t10\taaa\t12787\tbbb\t12787\t100.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1910\taaa ccc\t10\taaa\t12787\tccc\t12787\t200.9316017295349717"));
+		mapReduceDriver.withInput(new Text(""), new Text("1910\taaa ddd\t10\taaa\t12787\tddd\t12787\t300.9316017295349717"));
+
+		mapReduceDriver.withOutput(new Text("1900\t-0.9316017295349717\taaa ddd"),new Text(""));
+		mapReduceDriver.withOutput(new Text("1900\t-1.9316017295349717\taaa ccc"),new Text(""));
+
+		mapReduceDriver.withOutput(new Text("1910\t300.9316017295349717\taaa ddd"),new Text(""));
+		mapReduceDriver.withOutput(new Text("1910\t200.9316017295349717\taaa ccc"),new Text(""));
+
+		mapReduceDriver.runTest();
+
 	}
 
 }
